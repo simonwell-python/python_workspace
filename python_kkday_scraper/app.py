33c1eff3c5840a9f2e440c49312fae28514c7275
simon <simonwell.ios@gmail.com>
2021-12-31 17:07:24 +0800

    更新

---
+++ python_kkday_scraper/app.py
@@ -0,0 +1,3 @@
+# 串列可以存放不同資料型態的資料
+numbers = [1, 2, 3, 4]
+numbers = ["perter", "simon", "john"]
---
+++ python_kkday_scraper/scrapers.py
@@ -0,0 +1,9 @@
+import requests
+
+
+# KKday網站
+class Kkday:
+
+    # 建構式
+    def __init__(self, city_name):
+        self.city_name = city_name  # 城市名稱屬性
